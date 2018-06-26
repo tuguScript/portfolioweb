92a7bdf0796a968c94371e9376f3139357c9d80f
tuguscript <tuguscript@gmail.com>
2018-06-25 22:42:13 -0700

    header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import 'font-awesome/css/font-awesome.min.css';
-import './App.css';
+import "font-awesome/css/font-awesome.min.css";
+import "./App.css";
 import {
   NavLink,
   Redirect,
@@ -12,40 +12,68 @@ import About from "./views/About";
 import Works from "./views/Works";
 import Contact from "./views/Contact";
 import Sidebar from "./components/Sidebar/Sidebar";
+import Header from "./components/Header/Header";
+import injectSheet from "react-jss";
 
-const App = () => (
+const styles = {
+  container: {
+    display: "grid",
+    gridTemplateColumns: "256px 1fr",
+    gridTemplateRows: "auto",
+    gridTemplateAreas: `
+                  'sidebar header'
+                  'sidebar body'
+                  `
+  },
+  header: {
+    gridArea: "header"
+  },
+  body: {
+    gridArea: "body"
+  },
+  sidebar: {
+    gridArea: "sidebar"
+  }
+};
+
+const App = ({ classes }) => (
   <Router>
-    <div>
-      <Sidebar>
-        <NavLink
-          to="/about"
-          activeStyle={{
-            fontWeight: "bold",
-            color: "red"
-          }}
-        >
-          About
-        </NavLink>
-        <NavLink
-          to="/works"
-          activeStyle={{
-            fontWeight: "bold",
-            color: "red"
-          }}
-        >
-          Works
-        </NavLink>
-        <NavLink
-          to="/contact"
-          activeStyle={{
-            fontWeight: "bold",
-            color: "red"
-          }}
-        >
-          Contact
-        </NavLink>
-      </Sidebar>
-      <div className="body">
+    <div className={classes.container}>
+      <div className={classes.header}>
+        <Header />
+      </div>
+      <div className={classes.sidebar}>
+        <Sidebar>
+          <NavLink
+            to="/about"
+            activeStyle={{
+              fontWeight: "bold",
+              color: "red"
+            }}
+          >
+            About
+          </NavLink>
+          <NavLink
+            to="/works"
+            activeStyle={{
+              fontWeight: "bold",
+              color: "red"
+            }}
+          >
+            Works
+          </NavLink>
+          <NavLink
+            to="/contact"
+            activeStyle={{
+              fontWeight: "bold",
+              color: "red"
+            }}
+          >
+            Contact
+          </NavLink>
+        </Sidebar>
+      </div>
+      <div className={classes.body}>
         <Route exact path="/" render={() => <Redirect to="/about" />} />
         <Route exact path="/about" component={About} />
         <Route path="/works" component={Works} />
@@ -54,4 +82,7 @@ const App = () => (
     </div>
   </Router>
 );
-export default App;
+
+const StyledApp = injectSheet(styles)(App);
+
+export default StyledApp;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,14 @@
 import React, { Component } from "react";
+import injectSheet from "react-jss";
 
-const Header = () => <div className="header">qwe</div>;
+const styles = {
+  header: {
+    height: '64px'
+  }
+};
+
+const Header0 = ({ classes }) => <div className={classes.header}>header</div>;
+
+const Header = injectSheet(styles)(Header0);
 
 export default Header;
