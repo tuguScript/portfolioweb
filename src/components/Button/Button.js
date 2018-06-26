2d4252a594850413caeea02a2abf5959620ec593
Tuguldur Bayarnyam <tuguldur@Tuguldurs-MacBook-Pro.local>
2018-06-25 17:16:30 -0700

    added send email feature

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,24 +2,33 @@ import React, { Component } from "react";
 import AntButton from "antd/lib/button";
 import colors from "../../utils/colors";
 
-const Button = ({ children, type }) => {
-  if (type == "primary") {
+const Button = (props, { children, type, onClick }) => {
+  if (props.type == "primary") {
     return (
-      <AntButton style={{ backgroundColor: colors.PRIMARY }}>
-        <a style={{ color: "white" }}>{children}</a>
+      <AntButton
+        style={{ backgroundColor: colors.PRIMARY }}
+        onClick={() => props.onClick()}
+      >
+        <a style={{ color: "white" }}>{props.children}</a>
       </AntButton>
     );
   }
-  if (type == "secondary") {
+  if (props.type == "secondary") {
     return (
-      <AntButton style={{ backgroundColor: colors.SECONDARY }}>
-        <a style={{ color: "white" }}>{children}</a>
+      <AntButton
+        style={{ backgroundColor: colors.SECONDARY }}
+        onClick={() => props.onClick()}
+      >
+        <a style={{ color: "white" }}>{props.children}</a>
       </AntButton>
     );
   } else {
     return (
-      <AntButton style={{ backgroundColor: "black" }}>
-        <a style={{ color: "white" }}>{children}</a>
+      <AntButton
+        style={{ backgroundColor: "black" }}
+        onClick={() => props.onClick()}
+      >
+        <a style={{ color: "white" }}>{props.children}</a>
       </AntButton>
     );
   }
--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,23 +1,72 @@
-import React from "react";
+import React, { Component } from "react";
 import Footer from "../components/Footer/Footer";
 import { Input } from "antd";
-import Button from '../components/Button/Button'
+import Button from "../components/Button/Button";
+import axios from "axios";
 const { TextArea } = Input;
 
-const Contact = () => {
-  return (
-    <div>
-      <h1>Contact</h1>
-      <div className="caption">Email:</div>
-      <TextArea placeholder="..." autosize />
-      <div style={{ margin: "24px 0" }} />
-      <div className="caption">Text:</div>
-      <TextArea placeholder="..." autosize={{ minRows: 2, maxRows: 6 }} />
-      <Button type="primary">Send</Button>
-      <Button type="secondary">Clean</Button>
-      <Footer />
-    </div>
-  );
-};
+export default class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.sendEmail = this.sendEmail.bind(this);
+    this.emailTextInput = null;
 
-export default Contact;
+    this.setEmailTextInputRef = element => {
+      this.emailTextInput = element;
+    };
+  }
+  componentDidMount = () => {
+    this.emailTextInput.focus();
+    console.log(this.emailTextInput)
+  };
+  sendEmail() {
+    if (
+      this.state.emailTextInput === "" &&
+      this.state.messageTextInput === ""
+    ) {
+      return;
+    }
+    axios
+      .post("https://formspree.io/tuguscript@gmail.com", {
+        data: {
+          email: this.state.emailTextInput,
+          message: this.state.messageTextInput
+        }
+      })
+      .then(function(response) {
+        console.log(response);
+        // form.reset()
+      })
+      .catch(function(error) {
+        console.log(error);
+        // form.reset()
+      });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Contact</h1>
+        <div className="caption">Email:</div>
+        <TextArea
+          ref={this.setEmailTextInputRef}
+          placeholder="..."
+          autosize
+          onChange={e => this.setState({ emailTextInput: e.target.value })}
+        />
+        <div style={{ margin: "24px 0" }} />
+        <div className="caption">Text:</div>
+        <TextArea
+          ref={this.textInput}
+          placeholder="..."
+          autosize={{ minRows: 2, maxRows: 6 }}
+          onChange={e => this.setState({ messageTextInput: e.target.value })}
+        />
+        <Button type="primary" onClick={() => this.sendEmail()}>
+          Send
+        </Button>
+        <Button type="secondary">Clean</Button>
+        <Footer />
+      </div>
+    );
+  }
+}
