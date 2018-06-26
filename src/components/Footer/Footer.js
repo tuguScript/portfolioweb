fa84396e6f833a85e8adc4c4ea32bf62ac230132
tuguscript <tuguscript@gmail.com>
2018-06-25 18:23:39 -0700

    added social links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 
 const Footer = () => (
   <div className="footer">
-    <i className="fa fab fa-facebook-f" />
-    <i className="fa fab fa-twitter" />
-    <i className="fa fab fa-linkedin" />
-    <i className="fa fab fa-github" />
-    <i className="fa fas fa-envelope" />
+    <i className="fa fab fa-facebook-f" onClick={() => window.open('https://www.facebook.com/Tuguldur01')}/>
+    <i className="fa fab fa-twitter" onClick={() => window.open("https://" + "twitter.com/tuguldur_01")}/>
+    <i className="fa fab fa-linkedin" onClick={() => window.open("https://www.linkedin.com/in/tuguldurtech/")}/>
+    <i className="fa fab fa-github" onClick={() => window.open("https://github.com/tuguScript")}/>
+    <i className="fa fas fa-envelope" onClick={() => window.open("mailto:tuguscript@gmail.com")}/>
   </div>
 );
 
